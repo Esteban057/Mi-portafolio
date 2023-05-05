bff6a7f9a448a3f362d6f4e6ec6194b12414f365
Esteban057 <ph6227800@gmail.com>
2023-05-05 11:32:53 -0500

    Menu hamburgesa

---
+++ Js/media-query.js
@@ -0,0 +1,7 @@
+let menuBar = document.getElementById('menuBar')
+
+menuBar.addEventListener('click', function(){
+    alert('Hola chabal')
+})
+
+alert('Estimado usuario bienbenido a JS esperamos que tenga una esperiencia Agradale en nuestra pagina');
