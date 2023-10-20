5b48905ee6ca999c31f1622aeb3b9f96ec056bc9
Esteban057 <ph6227800@gmail.com>
2023-10-20 07:26:04 -0500

    adregar carpetas

---
+++ js/media-query.js
@@ -0,0 +1,20 @@
+let mainNav = document.getElementById("mainNav");
+let menuBar = document.getElementById("menuBar");
+let mainMenu = document.getElementById("mainMenu");
+
+menuBar.addEventListener("click", function(){
+    /* alert("Buenas tardes") */;
+    if (mainMenu.classList.contains("main-menu")){
+        mainMenu.classList.remove("main-menu");
+    mainMenu.classList.add("main-menu-block");
+    }
+    else {
+        mainMenu.classList.add("main-menu")
+        mainMenu.classList.remove("main-menu-block")
+    }
+    mainNav.classList.remove("main-nav");
+    mainNav.classList.add("main-nav-2");
+    /* mainMenu.classList.remove("main-menu");
+    mainMenu.classList.add("main-menu-block"); */
+})
+alert("LOL")
